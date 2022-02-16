7fb2db0a68bb25fd53bbdde218bc473ff6617e82
lalala-1235 <56725270+lalala-1235@users.noreply.github.com>
2022-02-16 19:10:14 +0900

    커스텀 데미지 관련 기능 추가

--- src/main/kotlin/com/lalala1235/rpgproject/event/listeners/EventListeners.kt
+++ src/main/kotlin/com/lalala1235/rpgproject/event/listeners/EventListeners.kt
@@ -3,15 +3,17 @@ package com.lalala1235.rpgproject.event.listeners
 import com.lalala1235.rpgproject.Main
 import com.lalala1235.rpgproject.event.customevent.CustomDamageEvent
 import com.lalala1235.rpgproject.magic.DamageInfo
-import com.lalala1235.rpgproject.utils.NBTManipulation
+import com.lalala1235.rpgproject.utils.DamageCalculator
 import com.lalala1235.rpgproject.utils.PDCManipulation
 import org.bukkit.ChatColor
+import org.bukkit.Location
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.LivingEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.player.PlayerJoinEvent
 
 class EventListeners: Listener {
     @EventHandler
@@ -27,6 +29,18 @@ class EventListeners: Listener {
 
             e.isCancelled = true
         }
+
+        if(PDCManipulation.getTagString(e.damager, "isCustomPlayer")=="true") {
+            Main.getPlugin().server.pluginManager.callEvent(
+                CustomDamageEvent(
+                    "Custom Damage",
+                    DamageInfo(DamageCalculator.getDamage(PDCManipulation.getTagDouble(e.damager, "infoBaseDamage")!!, PDCManipulation.getTagDouble(e.damager, "infoStr")!!), e.damager, null),
+                    e.damager as LivingEntity,
+                    e.entity as LivingEntity)
+            )
+
+            e.isCancelled = true
+        }
     }
 
     @EventHandler
@@ -35,10 +49,11 @@ class EventListeners: Listener {
 
         val scheduler = Main.getPlugin().server.scheduler
 
-        e.entity.damage(1.0)
-        e.entity.damage(e.damageInfo.damage, e.damageInfo.damager)
+        e.entity.damage(e.damageInfo.damage)
 
-        val damageStand = e.entity.location.world!!.spawnEntity(e.entity.location, EntityType.ARMOR_STAND)
+        val newLoc = Location(e.entity.location.world, e.entity.location.x, e.entity.location.y - 0.7, e.entity.location.z)
+
+        val damageStand = e.entity.location.world!!.spawnEntity(newLoc, EntityType.ARMOR_STAND)
 
         damageStand.setGravity(false)
         damageStand.isInvulnerable = true
@@ -49,5 +64,14 @@ class EventListeners: Listener {
         scheduler.scheduleSyncDelayedTask(Main.getPlugin(), {
             damageStand.remove()
         }, 30L)
+    }
+
+    @EventHandler
+    fun onPlayerJoin(e: PlayerJoinEvent) {
+        if(PDCManipulation.getTagString(e.player, "isCustomPlayer")=="true") return
+
+        PDCManipulation.setTagString(e.player, "isCustomPlayer", "true")
+        PDCManipulation.setTagDouble(e.player, "infoBaseDamage", 1.0)
+        PDCManipulation.setTagDouble(e.player, "infoStr", 100.0)
     }
 }
---
+++ src/main/kotlin/com/lalala1235/rpgproject/utils/DamageCalculator.kt
@@ -0,0 +1,19 @@
+package com.lalala1235.rpgproject.utils
+
+import kotlin.math.log2
+
+class DamageCalculator {
+    companion object {
+        fun getDamage(baseDmg: Double, str: Double): Double {
+            return baseDmg * ((0.01 * str) + 1)
+        }
+
+        fun getDamage(baseDmg: Double, str: Double, def: Double): Double {
+            return (baseDmg * ((0.01 * str) + 1)) * (def / (def + 100))
+        }
+
+        fun getDamage(baseDmg: Double, str: Double, def: Double, multiplier: Double): Double {
+            return (baseDmg * ((0.01 * str) + 1)) * (def / (def + 100)) * multiplier
+        }
+    }
+}
